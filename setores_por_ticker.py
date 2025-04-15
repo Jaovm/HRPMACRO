8e3848bcc5e3153493196a9813b6cc99dbe070f2
Jaovm <122157844+Jaovm@users.noreply.github.com>
2025-04-15 19:04:49 -0300

    Add files via upload

---
+++ setores_por_ticker.py
@@ -0,0 +1,57 @@
+
+setores_por_ticker = {
+    # Bancos
+    'ITUB4.SA': 'Bancos', 'BBDC4.SA': 'Bancos', 'SANB11.SA': 'Bancos', 'BBAS3.SA': 'Bancos',
+    'ABCB4.SA': 'Bancos', 'BRSR6.SA': 'Bancos', 'BMGB4.SA': 'Bancos', 'BPAC11.SA': 'Bancos',
+
+    # Seguradoras
+    'BBSE3.SA': 'Seguradoras', 'PSSA3.SA': 'Seguradoras', 'SULA11.SA': 'Seguradoras', 'CXSE3.SA': 'Seguradoras',
+
+    # Bolsas e Serviços Financeiros
+    'B3SA3.SA': 'Bolsas e Serviços Financeiros', 'XPBR31.SA': 'Bolsas e Serviços Financeiros',
+
+    # Energia Elétrica
+    'EGIE3.SA': 'Energia Elétrica', 'CPLE6.SA': 'Energia Elétrica', 'TAEE11.SA': 'Energia Elétrica',
+    'CMIG4.SA': 'Energia Elétrica', 'AURE3.SA': 'Energia Elétrica', 'CPFE3.SA': 'Energia Elétrica',
+    'AESB3.SA': 'Energia Elétrica', 'ENBR3.SA': 'Energia Elétrica', 'TRPL4.SA': 'Energia Elétrica',
+
+    # Petróleo, Gás e Biocombustíveis
+    'PETR4.SA': 'Petróleo, Gás e Biocombustíveis', 'PRIO3.SA': 'Petróleo, Gás e Biocombustíveis',
+    'RECV3.SA': 'Petróleo, Gás e Biocombustíveis', 'RRRP3.SA': 'Petróleo, Gás e Biocombustíveis',
+    'UGPA3.SA': 'Petróleo, Gás e Biocombustíveis', 'VBBR3.SA': 'Petróleo, Gás e Biocombustíveis',
+
+    # Mineração e Siderurgia
+    'VALE3.SA': 'Mineração e Siderurgia', 'CSNA3.SA': 'Mineração e Siderurgia',
+    'GGBR4.SA': 'Mineração e Siderurgia', 'CMIN3.SA': 'Mineração e Siderurgia',
+    'GOAU4.SA': 'Mineração e Siderurgia', 'BRAP4.SA': 'Mineração e Siderurgia',
+
+    # Indústria e Bens de Capital
+    'WEGE3.SA': 'Indústria e Bens de Capital', 'RANI3.SA': 'Indústria e Bens de Capital',
+    'KLBN11.SA': 'Indústria e Bens de Capital', 'SUZB3.SA': 'Indústria e Bens de Capital',
+    'UNIP6.SA': 'Indústria e Bens de Capital', 'KEPL3.SA': 'Indústria e Bens de Capital',
+
+    # Agronegócio
+    'AGRO3.SA': 'Agronegócio', 'SLCE3.SA': 'Agronegócio', 'SMTO3.SA': 'Agronegócio', 'CAML3.SA': 'Agronegócio',
+
+    # Saúde
+    'HAPV3.SA': 'Saúde', 'FLRY3.SA': 'Saúde', 'RDOR3.SA': 'Saúde',
+    'QUAL3.SA': 'Saúde', 'RADL3.SA': 'Saúde',
+
+    # Tecnologia
+    'TOTS3.SA': 'Tecnologia', 'POSI3.SA': 'Tecnologia', 'LINX3.SA': 'Tecnologia', 'LWSA3.SA': 'Tecnologia',
+
+    # Consumo Discricionário
+    'MGLU3.SA': 'Consumo Discricionário', 'LREN3.SA': 'Consumo Discricionário',
+    'RENT3.SA': 'Consumo Discricionário', 'ARZZ3.SA': 'Consumo Discricionário',
+    'ALPA4.SA': 'Consumo Discricionário',
+
+    # Consumo Básico
+    'ABEV3.SA': 'Consumo Básico', 'NTCO3.SA': 'Consumo Básico', 'PCAR3.SA': 'Consumo Básico', 'MDIA3.SA': 'Consumo Básico',
+
+    # Comunicação
+    'VIVT3.SA': 'Comunicação', 'TIMS3.SA': 'Comunicação', 'OIBR3.SA': 'Comunicação',
+
+    # Utilidades Públicas
+    'SBSP3.SA': 'Utilidades Públicas', 'SAPR11.SA': 'Utilidades Públicas', 'CSMG3.SA': 'Utilidades Públicas',
+    'ALUP11.SA': 'Utilidades Públicas', 'CPLE6.SA': 'Utilidades Públicas'
+}
