fe7d7b54e5bee69c48fc63ce3510ce98075e560d
Jaovm <122157844+Jaovm@users.noreply.github.com>
2025-04-16 20:40:28 -0300

    Update HRPMACRO.py

--- HRPMACRO.py
+++ HRPMACRO.py
@@ -776,8 +776,6 @@ if st.button("Gerar Alocação Otimizada"):
             st.markdown(f"🔁 **Troco (não alocado):** R$ {troco:,.2f}")
 
 
-        else:
-                st.error("Falha na otimização da carteira.")
         except Exception as e:
             st.error(f"Erro na otimização: {str(e)}")
             
