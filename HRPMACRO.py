930d4e10d60cfb50f6e86123be5e062fa8af597a
Jaovm <122157844+Jaovm@users.noreply.github.com>
2025-04-22 19:24:28 -0300

    Update HRPMACRO.py

--- HRPMACRO.py
+++ HRPMACRO.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 import requests
+import datetime
 from datetime import datetime
 from sklearn.covariance import LedoitWolf
 from scipy.cluster.hierarchy import linkage, dendrogram
